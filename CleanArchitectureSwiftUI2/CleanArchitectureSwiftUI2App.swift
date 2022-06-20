b796d38ce8a4db68bdf445a1ee2aedf6f6ffeaf4
NyiYeHan <39417215+NyiYeHan@users.noreply.github.com>
2022-06-20 10:49:45 +0630

    Initial Commit

---
+++ CleanArchitectureSwiftUI2/CleanArchitectureSwiftUI2App.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchitectureSwiftUI2App.swift
+//  CleanArchitectureSwiftUI2
+//
+//  Created by Nyi Ye Han on 20/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct CleanArchitectureSwiftUI2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CleanArchitectureSwiftUI2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CleanArchitectureSwiftUI2
+//
+//  Created by Nyi Ye Han on 20/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
